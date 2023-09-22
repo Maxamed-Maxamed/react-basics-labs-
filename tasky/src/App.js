d743a8e7f3705eac8f2c459c5e3a0a33fac33760
Maxamed Maxamed <20088349@mail.wit.ie>
2023-09-22 12:23:08 +0100

    Completed Week 2 Lab

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -2,46 +2,76 @@
 
 import './App.css';
 import Task from './components/Task';
+import React, { useState } from 'react';
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 function App() {
+
+  
+  /* The code `const [ taskState, setTaskState ] = useState({ tasks: [...] });` is using the `useState`
+  hook in React to create a state variable called `taskState` and a function called `setTaskState`
+  to update the state. */
+  // const [ taskState, setTaskState ] = useState({
+  //   tasks: [
+  //     { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+  //     { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+  //     { title: "Tidy up", deadline: "Today" }
+  //   ]
+  // });
+  const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" }
+    ]
+  });
+
+  // return (
+  //   // <div className="App">
+  //   <div className="container">
+  //     <h1>Tasky</h1>
+
+      
+  //     <Task title="Dishes" deadline="Today" />
+  //     <Task title="Laundry" deadline="Tomorrow" />
+  //     <Task title="Tidy" deadline="Today" />
+  //     <Task title="Laundry" deadline="Tomorrow">
+  //       Fold laundry and put away
+  //   </Task>
+  //   </div>
+    
+  // );
   return (
-    // <div className="App">
     <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" />
-      <Task title="Laundry" deadline="Tomorrow" />
-      <Task title="Tidy" deadline="Today" />
-      <Task title="Laundry" deadline="Tomorrow">
-        Fold laundry and put away
-    </Task>
+      {/* {taskState.tasks.map((task) => (              
+    <Task 
+      title={task.title}
+      description={task.description}
+      deadline={task.deadline}
+    /> */}
+
+    {taskState.tasks.map((task) => (              
+      <Task 
+        title={task.title}
+        description={task.description}
+        deadline={task.deadline}
+        key={task.id}
+      />
+    ))}
+  
+  
+
+      {/* <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
+      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
+      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
+     */}
     </div>
-    
   );
-}
+  
+  }
+
 
 
 export default App;
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -16,8 +16,33 @@ const Task = (props) => {
             <p>{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.children}</p>
-            
         </div>
+
+
     )
+   
+  //     return (
+  //       <div className="container">
+  //         <h1>Tasky</h1>
+  //         {taskState.tasks.map((task) => (              
+  //   <Task 
+  //     title={task.title}
+  //     description={task.description}
+  //     deadline={task.deadline}
+  //   />
+  // ))} 
+  //         <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
+  //         <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
+  //         <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
+  //       </div>
+  //       );
+
+
+
+
+
+
+
+
 }
 export default Task;
