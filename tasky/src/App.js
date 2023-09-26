8f574c2df73b7948fadf9afe4f1c83324bbc36b1
Maxamed Maxamed <20088349@mail.wit.ie>
2023-09-26 14:10:45 +0100

    Week 3 Lab A done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
-
+import { v4 as uuidv4 } from 'uuid';
+import AddTaskForm from './components/Form';
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
@@ -19,42 +20,98 @@ function App() {
   //     { title: "Tidy up", deadline: "Today" }
   //   ]
   // });
-  const [ taskState ] = useState({
+
+  const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" },
-      {id: 4,  title:"unkown", description:"unknow",  deadline:"unknow"},
-      {id: 5,  title:"unkown",description:"unknow", deadline:"unknow"}
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false}
     ]
   });
 
-  // const [ taskState, setTaskState ] = useState({
-  //   tasks: [
-  //     { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-  //     { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-  //     { id: 3, title: "Tidy up", deadline: "Today" },
-  //     {id: 4,  title:"unkown", description:"unknow",  deadline:"unknow"},
-  //     {id: 5,  title:"unkown",description:"unknow", deadline:"unknow"}
-  //   ]
-  // });
+  
+  const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
 
+
+
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+  }
+
+
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+
+
+  const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+    console.log(formState);
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+    
+    tasks.push(form);
+    setTaskState({tasks});
+  }
 
   return (
-    <div className="container">
-      <h1>Tasky</h1>
     
-
-    {taskState.tasks.map((task) => (              
-      <Task 
-        title={task.title}
-        description={task.description}
-        deadline={task.deadline}
-        key={task.id}
-      />
-    ))}
-  
+      
+    
+      <div className="container">
+      <h1>Tasky</h1>
+      {taskState.tasks.map((task, index) => (              
+        <Task 
+          title={task.title}
+          description={task.description}
+          deadline={task.deadline}
+          key={task.id}
+          done={task.done}
+          markDone={() => doneHandler(index)}
+          deleteTask = {() => deleteHandler(index)}
+          // onChange={(event) => props.change(event)}
+          
+        />
+      ))}
+      <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+      
+      
     </div>
+      
+ 
+  
+   
   );
   
   }
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -11,15 +11,22 @@ import React from 'react';
 const Task = (props) => {
     
     return (
-        <div className="card">
+        // <div className="card">
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
              <p className="title">{props.title}</p>
             <p>{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.children}</p>
+
+            {/* <button onClick={props.markDone}>Done</button> */}
+            <button onClick={props.markDone} className='doneButton'>Done</button>
+            <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
+
         </div>
 
-
     )
+
+    
    
   //     return (
   //       <div className="container">
